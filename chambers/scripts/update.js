d31513cf85492f7a36ece52df508427ea278c749
Impcamper <impcamper@gmail.com>
2023-02-03 20:05:55 -0600

    messing with banner

--- chambers/scripts/update.js
+++ chambers/scripts/update.js
@@ -5,3 +5,8 @@ document.querySelector(
 const datefield = document.getElementById("date");
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
 datefield.innerHTML = `<em>${fulldate}</em>`;
+
+const date = new Date();
+var day= date.getDay();
+const banner = document.getElementById("banner");
+if(day==1 || day==2){banner.innerHTML="🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";}
