e52d8ba59b3f9fa8594936153e0a6fc93ec7665c
Impcamper <impcamper@gmail.com>
2023-02-05 00:04:59 -0600

    fixed date

--- chambers/scripts/update.js
+++ chambers/scripts/update.js
@@ -2,11 +2,13 @@ document.querySelector(
 	"#update"
 ).textContent = `Last Modification: ${document.lastModified}`;
 
+const date = new Date();
+
 const datefield = document.getElementById("date");
-const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(date);
 datefield.innerHTML = `<em>${fulldate}</em>`;
 
-const date = new Date();
+
 var day= date.getDay();
 const banner = document.getElementById("banner");
 if(day==1 || day==2){banner.innerHTML="🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";}
